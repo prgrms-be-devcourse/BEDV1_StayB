1061a467ffacdffab0c80384381a94c472eb2bd5
sunH0 <sunho6824@daum.net>
2021-10-29 02:47:51 +0900

    Feat: 개발환경 세팅

---
+++ src/test/java/org/programmers/staybb/StaybbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programmers.staybb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaybbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
