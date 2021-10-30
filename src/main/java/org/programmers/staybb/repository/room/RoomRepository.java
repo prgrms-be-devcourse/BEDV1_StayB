38c6da6afc71af1fbcc212fdc460a5bd76a9716a
BeautterLife <pgh9801@gmail.com>
2021-10-30 16:25:21 +0900

    feat/RoomRepository 구현

---
+++ src/main/java/org/programmers/staybb/repository/room/RoomRepository.java
@@ -0,0 +1,8 @@
+package org.programmers.staybb.repository.room;
+
+import org.programmers.staybb.domain.room.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Integer>  {
+
+}
