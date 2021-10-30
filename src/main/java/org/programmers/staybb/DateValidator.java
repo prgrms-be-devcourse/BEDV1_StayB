50e31054205559e90e0077d9c88be99448d7e345
sunH0 <sunho6824@daum.net>
2021-10-30 15:26:38 +0900

    Feat: Email을 검증해주는 @DateValid 구현

---
+++ src/main/java/org/programmers/staybb/DateValidator.java
@@ -0,0 +1,28 @@
+package org.programmers.staybb;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class DateValidator implements ConstraintValidator<DateValid, String> {
+
+    private String pattern;
+
+    @Override
+    public void initialize(DateValid constraintAnnotation) {
+        this.pattern = constraintAnnotation.pattern();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        try {
+            LocalDate localDate = LocalDate.parse(value + "01",
+                DateTimeFormatter.ofPattern(this.pattern));
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+}
