efb3d07780c7d3e504019d4f578324ee8261e98a
BeautterLife <pgh9801@gmail.com>
2021-10-30 16:27:01 +0900

    feat/RoomControllerTest 구현

---
+++ src/test/java/org/programmers/staybb/controller/room/RoomControllerTest.java
@@ -0,0 +1,25 @@
+package org.programmers.staybb.controller.room;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@AutoConfigureMockMvc
+@WebMvcTest
+class RoomControllerTest {
+
+  @Test
+  void save() {
+
+  }
+
+  @Test
+  void delete() {
+  }
+}
