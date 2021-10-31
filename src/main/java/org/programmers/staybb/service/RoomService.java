711bd437c13a8e3875990f04cb64d8c3e5c5bf9a
BeautterLife <pgh9801@gmail.com>
2021-10-31 14:03:08 +0900

    Refactor: save,delete시 roomId 반환하도록 변경

--- src/main/java/org/programmers/staybb/service/RoomService.java
+++ src/main/java/org/programmers/staybb/service/RoomService.java
@@ -1,9 +1,8 @@
-package org.programmers.staybb.service.room;
+package org.programmers.staybb.service;
 
 import javassist.NotFoundException;
-import org.programmers.staybb.converter.RoomConverter;
+import org.programmers.staybb.domain.room.Room;
 import org.programmers.staybb.dto.room.RoomRequestDto;
-import org.programmers.staybb.dto.room.RoomResponseDto;
 import org.programmers.staybb.repository.room.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,28 +11,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class RoomService {
 
-  private final RoomRepository roomRepository;
-  private final RoomConverter roomConverter;
+    private final RoomRepository roomRepository;
 
-  @Autowired
-  public RoomService(RoomRepository roomRepository, RoomConverter roomConverter) {
-    this.roomRepository = roomRepository;
-    this.roomConverter = roomConverter;
-  }
-
-
-  @Transactional
-  public RoomResponseDto save(RoomRequestDto roomDto) {
-    return roomConverter.converRoomResponseDto(
-        roomRepository.save(roomConverter.convertRoom(roomDto))
-    );
-  }
-
-  public void delete(int roomId) throws NotFoundException {
-    if (roomRepository.findById(roomId).isEmpty()) {
-      throw new NotFoundException("삭제하려는 숙소 정보가 없습니다.");
+    @Autowired
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
     }
-    roomRepository.deleteById(roomId);
-  }
+
+    @Transactional
+    public Long save(RoomRequestDto roomDto) {
+        return roomRepository.save(roomDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public long delete(int roomId) throws NotFoundException {
+        Room room = roomRepository.findById(roomId)
+            .orElseThrow(() -> new NotFoundException("해당 숙소 정보가 없습니다."));
+        roomRepository.deleteById(roomId);
+        return room.getId();
+    }
 
 }
