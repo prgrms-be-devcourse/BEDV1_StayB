53b75f54b15a4e9aabd32576677c022c3d3b56f7
BeautterLife <pgh9801@gmail.com>
2021-10-31 14:01:37 +0900

    Feat: Dto->Entity 변경 메소드 추가

--- src/main/java/org/programmers/staybb/dto/room/RoomRequestDto.java
+++ src/main/java/org/programmers/staybb/dto/room/RoomRequestDto.java
@@ -4,13 +4,14 @@ import lombok.Builder;
 import lombok.Getter;
 import org.programmers.staybb.domain.room.Address;
 import org.programmers.staybb.domain.room.Option;
-import org.programmers.staybb.service.room.RoomService;
+import org.programmers.staybb.domain.room.Room;
+import org.programmers.staybb.repository.HostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Getter
 public class RoomRequestDto {
 
-  private int userId;
+  private long hostId;
   private String roomName;
   private int maxGuest;
   private int price;
@@ -19,19 +20,32 @@ public class RoomRequestDto {
   private Option option;
   private Address address;
 
+
   @Autowired
-  private RoomService roomService;
+  private HostRepository hostRepository;
 
   @Builder
-  public RoomRequestDto(int userId, String roomName, int maxGuest, int price, String description,
+  public RoomRequestDto(int hostId, String roomName, int maxGuest, int price, String description,
       Option option, Address address) {
-    this.userId = userId;
+    this.hostId = hostId;
     this.roomName = roomName;
     this.maxGuest = maxGuest;
     this.price = price;
     this.description = description;
     this.option = option;
     this.address = address;
+  }
+
+  public Room toEntity() {
+    return Room.builder()
+        .host(hostRepository.getById(this.getHostId()))
+        .roomName(this.getRoomName())
+        .maxGuest(this.getMaxGuest())
+        .price(this.getPrice())
+        .description(this.getDescription())
+        .option(this.getOption())
+        .address(this.getAddress())
+        .build();
   }
 
 
