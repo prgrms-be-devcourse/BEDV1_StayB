4057b041c63398d5885d018102e42c564c5289ec
bnminji <bnminji@gmail.com>
2021-11-01 17:06:16 +0900

    git commit -m "Refactor: 베이스 코드 리팩토링"

---
+++ src/main/java/org/programmers/staybb/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.programmers.staybb.repository;
+
+import org.programmers.staybb.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
