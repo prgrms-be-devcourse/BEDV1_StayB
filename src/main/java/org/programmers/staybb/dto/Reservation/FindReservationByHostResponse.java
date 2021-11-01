bec146be5b9e7aa07ea341d57794868ddc0dab96
sunH0 <sunho6824@daum.net>
2021-11-01 20:14:43 +0900

    Feat: 예약 Dto 구현

---
+++ src/main/java/org/programmers/staybb/dto/Reservation/FindReservationByHostResponse.java
@@ -0,0 +1,42 @@
+package org.programmers.staybb.dto.Reservation;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import org.programmers.staybb.domain.reservation.Guest;
+import org.programmers.staybb.domain.reservation.Reservation;
+
+@Getter
+@Builder
+public class FindReservationByHostResponse {
+
+    private final Long id;
+    private final Long userId;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+    private final Guest guest;
+    private final int totalPrice;
+    private final String roomName;
+    private final String userName;
+    private final String userPhoneNumber;
+    private final String message;
+    private final LocalDateTime createdAt;
+
+
+    public static FindReservationByHostResponse of(Reservation entity) {
+        return FindReservationByHostResponse.builder()
+            .id(entity.getId())
+            .userId(entity.getUser().getId())
+            .startDate(entity.getStartDate())
+            .endDate(entity.getEndDate())
+            .guest(entity.getGuest())
+            .totalPrice(entity.getTotalPrice())
+            .roomName(entity.getRoom().getRoomName())
+            .userName(entity.getUser().getName())
+            .userPhoneNumber(entity.getUser().getPhoneNumber())
+            .build();
+
+    }
+
+}
