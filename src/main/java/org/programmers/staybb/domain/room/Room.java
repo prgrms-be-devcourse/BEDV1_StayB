0a1de33fb6386ebce064f884135b03ee593aea5e
BeautterLife <pgh9801@gmail.com>
2021-11-03 21:29:23 +0900

    Feat: 단일 필드 변경 메소드 구현

--- src/main/java/org/programmers/staybb/domain/room/Room.java
+++ src/main/java/org/programmers/staybb/domain/room/Room.java
@@ -1,16 +1,28 @@
 package org.programmers.staybb.domain.room;
 
 import com.sun.istack.NotNull;
+import java.lang.reflect.Field;
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import org.programmers.staybb.domain.user.Host;
-
-import javax.persistence.*;
 import org.programmers.staybb.global.BaseTimeEntity;
 
+@Getter
 @Entity
 @Table(name = "room")
-@Getter
 public class Room extends BaseTimeEntity {
 
     @Id
@@ -69,6 +81,12 @@ public class Room extends BaseTimeEntity {
     public void addRoom(Host host) {
         host.getRooms().add(this);
         this.host = host;
+    }
+
+    public void setField(String fieldToChange, Object value)
+        throws IllegalAccessException, NoSuchFieldException {
+        Field field = this.getClass().getDeclaredField(fieldToChange);
+        field.set(this, value);
     }
 
 }
