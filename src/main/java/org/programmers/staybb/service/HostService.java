6be40cb73e3f8fd618dea53421e77474887e7136
BeautterLife <pgh9801@gmail.com>
2021-11-05 02:02:40 +0900

    Refactor: annotation 순서 변경

--- src/main/java/org/programmers/staybb/service/HostService.java
+++ src/main/java/org/programmers/staybb/service/HostService.java
@@ -14,8 +14,8 @@ import org.programmers.staybb.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
 @Transactional
+@Service
 public class HostService {
 
     private final UserRepository userRepository;
