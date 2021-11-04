9eb366a7aadfa4aa9891fcdd942ac2c1f93ce622
BeautterLife <pgh9801@gmail.com>
2021-11-05 02:04:12 +0900

    Refactor: Entity single field update(patch) 오류 수정

--- src/main/java/org/programmers/staybb/service/RoomService.java
+++ src/main/java/org/programmers/staybb/service/RoomService.java
@@ -1,16 +1,20 @@
 package org.programmers.staybb.service;
 
-import java.util.Map;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.lang.reflect.Field;
+import javax.persistence.Embedded;
+import org.json.JSONObject;
 import org.programmers.staybb.domain.room.Room;
 import org.programmers.staybb.domain.user.Host;
 import org.programmers.staybb.dto.room.RoomDetailResponse;
+import org.programmers.staybb.dto.room.RoomIdResponse;
 import org.programmers.staybb.dto.room.RoomRequest;
 import org.programmers.staybb.dto.room.RoomSummaryResponse;
 import org.programmers.staybb.global.exception.EntityNotFoundException;
 import org.programmers.staybb.global.exception.ErrorCode;
 import org.programmers.staybb.repository.HostRepository;
 import org.programmers.staybb.repository.RoomRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -22,24 +26,26 @@ public class RoomService {
 
     private final RoomRepository roomRepository;
     private final HostRepository hostRepository;
+    private final ObjectMapper objectMapper;
 
-    @Autowired
-    public RoomService(RoomRepository roomRepository, HostRepository hostRepository) {
+    public RoomService(RoomRepository roomRepository, HostRepository hostRepository,
+        ObjectMapper objectMapper) {
         this.roomRepository = roomRepository;
         this.hostRepository = hostRepository;
+        this.objectMapper = objectMapper;
     }
 
-    public Long save(final RoomRequest roomRequest) throws EntityNotFoundException {
+    public RoomIdResponse save(final RoomRequest roomRequest) throws EntityNotFoundException {
         Host host = hostRepository.findById(roomRequest.getHostId())
             .orElseThrow(() -> new EntityNotFoundException(ErrorCode.HOST_NOT_FOUND));
-        return roomRepository.save(roomRequest.toEntity(host)).getId();
+        return new RoomIdResponse(roomRepository.save(roomRequest.toEntity(host)).getId());
     }
 
-    public Long delete(final Long roomId) throws EntityNotFoundException {
+    public RoomIdResponse delete(final Long roomId) throws EntityNotFoundException {
         Room room = roomRepository.findById(roomId)
             .orElseThrow(() -> new EntityNotFoundException(ErrorCode.ROOM_NOT_FOUND));
         roomRepository.delete(room);
-        return room.getId();
+        return new RoomIdResponse(room.getId());
     }
 
     @Transactional(readOnly = true)
@@ -55,15 +61,21 @@ public class RoomService {
         return roomPage.map(RoomSummaryResponse::of);
     }
 
-    @Transactional
-    public Long updateSingleField(Long roomId, Map<String, Object> updateInfo)
-        throws NoSuchFieldException, IllegalAccessException, EntityNotFoundException {
+    public RoomIdResponse updateSingleField(final Long roomId, final String field,
+        final String value)
+        throws EntityNotFoundException, NoSuchFieldException, IllegalAccessException, JsonProcessingException {
         Room room = roomRepository.findById(roomId)
             .orElseThrow(() -> new EntityNotFoundException(ErrorCode.ROOM_NOT_FOUND));
 
-        String fieldToChange = updateInfo.keySet().toArray()[0].toString();
-        room.setField(fieldToChange, updateInfo.get(fieldToChange));
+        Field fieldToUpdate = room.getClass().getDeclaredField(field);
 
-        return room.getId();
+        if (fieldToUpdate.isAnnotationPresent(Embedded.class)) {
+            room.setField(field, objectMapper.readValue(value, fieldToUpdate.getType()));
+        } else {
+            JSONObject info = new JSONObject(value);
+            room.setField(field, info.get(field));
+        }
+        return new RoomIdResponse(room.getId());
     }
+
 }
