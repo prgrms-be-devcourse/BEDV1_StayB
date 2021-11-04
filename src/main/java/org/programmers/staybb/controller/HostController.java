c12edf328340d667e562ffddf1eebfc95b2e9302
BeautterLife <pgh9801@gmail.com>
2021-11-04 23:42:49 +0900

    Refactor: annotaion 순서 변경

--- src/main/java/org/programmers/staybb/controller/HostController.java
+++ src/main/java/org/programmers/staybb/controller/HostController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequestMapping("/v1/host")
+@RestController
 public class HostController {
 
     private final HostService hostService;
