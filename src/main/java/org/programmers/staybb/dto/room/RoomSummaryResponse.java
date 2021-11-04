7563dd69eaa8520d5b205c6e88e672bd887d0bc5
BeautterLife <pgh9801@gmail.com>
2021-11-04 13:12:01 +0900

    Refactor: 클래스명 변경

--- src/main/java/org/programmers/staybb/dto/room/RoomSummaryResponse.java
+++ src/main/java/org/programmers/staybb/dto/room/RoomSummaryResponse.java
@@ -7,21 +7,21 @@ import org.programmers.staybb.domain.room.Option;
 import org.programmers.staybb.domain.room.Room;
 
 @Getter
-public class RoomResponse {
+public class RoomSummaryResponse {
 
     private String address;
     private Option option;
     private LocalDateTime updatedAt;
 
     @Builder
-    private RoomResponse(String address, Option option, LocalDateTime updatedAt) {
+    private RoomSummaryResponse(String address, Option option, LocalDateTime updatedAt) {
         this.address = address;
         this.option = option;
         this.updatedAt = updatedAt;
     }
 
-    public static RoomResponse of(Room entity) {
-        return RoomResponse.builder()
+    public static RoomSummaryResponse of(Room entity) {
+        return RoomSummaryResponse.builder()
             .address(entity.getAddress().getAddress())
             .option(entity.getOption())
             .updatedAt(entity.getUpdatedAt())
