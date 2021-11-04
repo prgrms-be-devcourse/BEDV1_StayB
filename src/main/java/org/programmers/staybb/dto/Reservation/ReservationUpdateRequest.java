cc5b95a12a212ab6c082fa7797d3aa9edb770564
sunH0 <sunho6824@daum.net>
2021-11-05 06:34:08 +0900

    Fix: UpdateRequest 체크인 체크아웃 필드 @Past -> @Future 수정

--- src/main/java/org/programmers/staybb/dto/Reservation/ReservationUpdateRequest.java
+++ src/main/java/org/programmers/staybb/dto/Reservation/ReservationUpdateRequest.java
@@ -2,8 +2,8 @@ package org.programmers.staybb.dto.Reservation;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
+import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -13,12 +13,12 @@ public class ReservationUpdateRequest {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @NotNull(message = "체크인 날짜를 입력해주세요.")
-    @Past(message = "체크인 날짜를 다시 입력해주세요.")
+    @Future(message = "체크인 날짜를 다시 입력해주세요.")
     private final LocalDate startDate;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @NotNull(message = "체크아웃 날짜를 입력해주세요.")
-    @Past(message = "체크아웃 날짜를 다시 입력해주세요.")
+    @Future(message = "체크아웃 날짜를 다시 입력해주세요.")
     private final LocalDate endDate;
 
     @NotNull(message = "숙박 인원을 입력해주세요.")
