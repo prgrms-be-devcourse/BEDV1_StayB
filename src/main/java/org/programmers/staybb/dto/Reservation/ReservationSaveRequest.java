3fcb43bbfe6f8829b2fcef45fd3ebc86b9a44ed7
sunH0 <sunho6824@daum.net>
2021-11-05 05:20:50 +0900

    Fix: Request LocalData 필드 매핑 오류 수정

--- src/main/java/org/programmers/staybb/dto/Reservation/ReservationSaveRequest.java
+++ src/main/java/org/programmers/staybb/dto/Reservation/ReservationSaveRequest.java
@@ -18,12 +18,12 @@ import org.programmers.staybb.domain.user.User;
 @Builder
 public class ReservationSaveRequest {
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMdd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @NotNull(message = "체크인 날짜를 입력해주세요.")
     @Past(message = "체크인 날짜를 다시 입력해주세요.")
     private final LocalDate startDate;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMdd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @NotNull(message = "체크아웃 날짜를 입력해주세요.")
     @Past(message = "체크아웃 날짜를 다시 입력해주세요.")
     private final LocalDate endDate;
